c068e0bcad3f60a5c2f092b377f4da24d9085d3e
Yufeng Zhu <yufeng.zhu@ge.com>
2017-04-22 20:03:25 -0700

    updated count of range sum

--- CountofRangeSum.java
+++ CountofRangeSum.java
@@ -10,28 +10,34 @@ public class CountofRangeSum {
     //https://discuss.leetcode.com/topic/33738/share-my-solution/2
     //http://www.cnblogs.com/EdwardLiu/p/5138198.html
     //还有树状数组（Fenwick Tree）解法,懒得看了
+    public static void main(String[] args){
+        CountofRangeSum cr=new CountofRangeSum();
+        int[] n={2147483647,-2147483648,-1,0};
+        System.out.println(cr.countRangeSum(n,-1,0));
+    }
+
     int rs=0;
     public int countRangeSum(int[] nums, int lower, int upper) {
 
         if(nums==null||nums.length==0){
             return 0;
         }
-        long sum[]=new long[nums.length+1];
-        for(int i=1;i<nums.length;i++){
-            sum[i]=sum[i-1]+nums[i];
+        long sum[]=new long[nums.length+1];//这里也要想一下,这个和的数组要比原素组多一位,并把第一位作为0,那么mergesort的时候肯定会merge第0和1位merge,
+        for(int i=1;i<=nums.length;i++){    // sum[1]-sum[0]就确实会算上nums[0]到nums[0],如果符合nums[0]符合条件的话.即sum数组第一位是0的意义是让右边
+            sum[i]=sum[i-1]+nums[i-1];     //的数组有机会减去这个0即得到他自身的值的机会
         }
-        devideAndconquer(sum,0,nums.length,lower,upper);
+        devideAndconquer(sum,0,nums.length,(long)lower,(long)upper);
         return rs;
 
     }
-    public long[] devideAndconquer(long[] sum,int b,int e,int low,int up){
+    public long[] devideAndconquer(long[] sum,int b,int e,long low,long up){
         if(b>e){
             //好像不会出现这种情况,b=e时就处理掉了
             return new long[0];
         }
         long[] temp=new long[e-b+1];
         if(e==b){
-            temp[0]=sum[0];
+            temp[0]=sum[b];
             return temp;
         }
         int mid=(b+e)/2;
@@ -41,59 +47,37 @@ public class CountofRangeSum {
 
 
     }
-    public long[] merge(long[] l,long[] r,int low,int up){
+    public long[] merge(long[] l,long[] r,long low,long up){
         //好像不会有l或者r长度为0的情况
-        int m=-1;
-        int n=-1;
+        int m=0;
+        int n=0;
         int b=0;
         int e=0;
         int mid=0;
         long[] temp=new long[l.length+r.length];
-        for(int i=0;i<l.length;i++) {//用二分法找到m和n,m-n就是符合条件的个数
-            //找右边点
-            b=0;
-            e=r.length-1;
-            while (b < e) {
-                mid = (b + e) / 2;
-                if ((r[mid] - l[i] > up && mid - 1 >= 0 && r[mid - 1] - l[i] <= up) || mid == 0 && (r[mid] - l[i] > up)) {
-                    //m is the first index satisfy sums[j] - sums[i] > upper and
-                    //n is the first index satisfy sums[k] - sums[i] >= lower.  然后m-n就得出这段区间符合条件的个数,
-                    // 自己举个例子,假如0,1,2,3四个数,r[3]-l[i]的时候就大于up了,说明3时零界点,如果r[1]-l[i]>=low说明到r数
-                    //组中index为1,2即2个数符合条件,即3-1=2个
-                    m = mid;
-                    break;
-                }
-                if (r[mid] - l[i] <= up) {
-                    b = mid + 1;
-                } else {
-                    e = mid;
-                }
+        for(int i=0;i<l.length;i++) {
+            if(r.length==1&&r[0]-l[i]>=low&&r[0]-l[i]<=up){
+                rs++;
+                continue;
             }
-            //找左边点
-            b=0;
-            e=r.length-1;
-            while (b < e) {
-                mid = (b + e) / 2;
-                if ((r[mid] - l[i] >= low && mid - 1 >= 0 && r[mid - 1] - l[i] < low) || mid == 0 && (r[mid] - l[i] >= low)) {
-                    //m is the first index satisfy sums[j] - sums[i] > upper and
-                    //n is the first index satisfy sums[k] - sums[i] >= lower.  然后m-n就得出这段区间符合条件的个数,
-                    // 自己举个例子,假如0,1,2,3四个数,r[3]-l[i]的时候就大于up了,说明3时零界点,如果r[1]-l[i]>=low说明到r数
-                    //组中index为1,2即2个数符合条件,即3-1=2个
-                    n = mid;
-                    break;
-                }
-                if (r[mid] - l[i] <= low) {
-                    b = mid + 1;
-                } else {
-                    e = mid;
-                }
+            m=0;
+            n=0;
+            //m is the first index satisfy sums[m] - sums[i] > upper and
+            //n is the first index satisfy sums[n] - sums[i] >= lower.  然后m-n就得出这段区间符合条件的个数,
+            // 自己举个例子,假如0,1,2,3四个数,r[3]-l[i]的时候就大于up了,说明3时零界点,如果r[1]-l[i]>=low说明到r数
+            //组中index为1,2即2个数符合条件,即3-1=2个
+            //开始想用用二分法找到m和n,m-n就是符合条件的个数,但是有一种情况不好处理就是比如r数组是{2,2},然后这两个数都符合r-l[i]符合low和up,那么二分法
+            //得出的m就是1,实际上应该是2才对,所以只能线性查找了,然而就超时了,运行第二次又accept了,然后多次都超时,说明基本上是快超时的,估计是devideAndconquer
+            //里面创建新数组花的时间吧,人家用的system.copyarray貌似性能快
+
+            while(m<r.length&&r[m]-l[i]<=up){
+                m++;
             }
-            if(m==-1){
-                m=r.length;
-            }
-            if(n==-1){
-                n=l.length;
-            }
+
+            //找左边点,即从左到右第一个下标n,使得ums[n-1]-sums[i]<low,且sums[n]-sums[i]>=low,实际上和m点一起看来,是左闭右开,使得m-n就是区间中间符合条件的个数
+           while(n<r.length&&r[n]-l[i]<low){
+               n++;
+           }
             rs+=m-n;
 
         }
@@ -103,20 +87,16 @@ public class CountofRangeSum {
         int t=0;
         while(p<l.length&&q<r.length){
             if(l[p]<=r[q]){
-                temp[t]=r[q];
-                q++;
+                temp[t++]=l[p++];
             }else{
-                temp[t]=l[p];
+                temp[t++]=r[q++];
             }
-            t++;
         }
         while(p!=l.length){
-            temp[t]=l[p];
-            p++;
+            temp[t++]=l[p++];
         }
         while(q!=r.length){
-            temp[t]=r[q];
-            q++;
+            temp[t++]=r[q++];
         }
         return temp;
 
