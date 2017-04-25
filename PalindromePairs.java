99d425636676dad21b7ce41626c4fdae30076bea
Yufeng Zhu <yufeng.zhu@ge.com>
2017-04-25 11:19:17 -0700

    added TopKFrequentElements and PalindromePairs

---
+++ PalindromePairs.java
@@ -0,0 +1,120 @@
+import java.util.*;
+
+/**
+ * Created by 502575560 on 4/24/17.
+ */
+public class PalindromePairs {
+    public static void main(String[] args){
+        String[] words={"bat", "tab", "cat"};
+        palindromePairs(words);
+    }
+    //自己只能想暴力法的
+////这题有空字符串的话很麻烦,逻辑写起来很不好看.记着吧
+    //http://blog.csdn.net/z6491679/article/details/51043225
+    //http://blog.csdn.net/xiexingshishu/article/details/51056177
+    //http://blog.csdn.net/lyh642784803/article/details/51834086
+    //https://segmentfault.com/a/1190000005605192  代码看这个的,这个还有tire的做法,没看,他的hashmap方法写的好,不像我写的这么复杂
+    public static List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> rs=new ArrayList<>();
+        if(words.length<=1){
+            return rs;
+        }
+        HashMap<String,Integer> map=new HashMap<>();
+        for(int i=0;i<words.length;i++){
+            map.put(words[i],i);
+        }
+        if(map.containsKey("")){//我开始想着先处理全部空字符的情况,但是这样的话下面的逻辑会重复
+            for(int i=0;i<words.length;i++){
+                if(!words[i].equals("")&&isPalindorme(words[i])){
+                    List<Integer> ls=new ArrayList<>();
+                    ls.add(map.get(""));
+                    ls.add(map.get(words[i]));
+                    List<Integer> ls2=new ArrayList<>();
+                    ls2.add(map.get(words[i]));
+                    ls2.add(map.get(""));
+                    rs.add(ls);
+                    rs.add(ls2);
+                }
+            }
+        }
+        for(int i=0;i<words.length;i++){
+            String w=words[i];
+            if(w.equals("")){//上面处理了""的情况,这里不处理了
+                continue;
+            }
+            for(int j=0;j<w.length();j++){//这里限制了j<w.length,所以只可能出现left是""的情况,不会出现right是""的情况,right最少也有一个字母
+                String left=w.substring(0,j);
+                String right=w.substring(j,w.length());
+                String rleft=reverse(left);
+                String rright=reverse(right);
+                if(isPalindorme(left)&&map.containsKey(rright)&&map.get(rright)!=map.get(w)){//左边是对称,即rright|left|right的情
+                    List<Integer> ls=new ArrayList<>();                             //况,并且rright不能是他自己,否则如果words[i]是单个字母的字符串会被加入结果
+                    ls.add(map.get(rright));
+                    ls.add(map.get(w));
+                    rs.add(ls);
+                }   //left|right|rleft的情况,这里!rleft.equals("")的条件对付了{"a",""}的情况,因为上面单独处理""的for循环已经处理了""+"a"+""的情况,所以这里要排除
+                if(isPalindorme(right)&&!rleft.equals("")&&map.containsKey(rleft)&&map.get(rleft)!=map.get(w)){
+                    List<Integer> ls=new ArrayList<>();
+                    ls.add(map.get(w));
+                    ls.add(map.get(rleft));
+                    rs.add(ls);
+                }
+            }
+        }
+        return rs;
+    }
+    public static boolean isPalindorme(String s){
+        if(s==""){
+            return true;
+        }
+        int b=0;
+        int e=s.length()-1;
+        while(b<e){
+            if(s.charAt(b)!=s.charAt(e)){
+                return false;
+            }
+            b++;
+            e--;
+        }
+        return true;
+
+    }
+    private static String reverse(String s)
+    {
+        StringBuilder sb = new StringBuilder(s);
+        return sb.reverse().toString();
+    }
+
+    //https://segmentfault.com/a/1190000005605192 的代码,注意见下面
+    public List<List<Integer>> palindromePairs2(String[] words) {
+        List<List<Integer>> ret = new ArrayList<>();
+        if (words == null || words.length < 2) return ret;
+        Map<String, Integer> map = new HashMap<>();
+        for (int i=0; i<words.length; i++) map.put(words[i], i);
+        for (int i=0; i<words.length; i++) {
+            for (int j=0; j<=words[i].length(); j++) { // 注意他这里 j <= words[i],而我的是j<words[i]
+                String str1 = words[i].substring(0, j);
+                String str2 = words[i].substring(j);
+                if (isPalindrome(str1)) {
+                    String str2rvs = new StringBuilder(str2).reverse().toString();
+                    if (map.getOrDefault(str2rvs, i) != i) ret.add(Arrays.asList(map.get(str2rvs), i));
+                }
+                if (isPalindrome(str2) && str2.length() != 0) {//他这里加了右边的length不等于0这个条件,否则会重复,拿["bat", "tab"]想
+                    String str1rvs = new StringBuilder(str1).reverse().toString();
+                    // check "str.length() != 0" to avoid duplicates
+                    if (map.getOrDefault(str1rvs, i) != i) ret.add(Arrays.asList(i, map.get(str1rvs)));
+                }
+            }
+        }
+        return ret;
+    }
+    private boolean isPalindrome(String str) {
+        for (int l = 0, r = str.length() - 1; l <= r; l ++, r --)
+            if (str.charAt(l) != str.charAt(r)) return false;
+        return true;
+    }
+
+
+
+}
+
---
+++ TopKFrequentElements.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+/**
+ * Created by 502575560 on 4/23/17.
+ */
+public class TopKFrequentElements {
+
+    //自己想的,不是太顺
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> rs=new ArrayList<>();
+        if(nums.length==0){
+            return rs;
+        }
+        HashMap<Integer,struct2> map=new HashMap<>();
+        PriorityQueue<struct2> pq=new PriorityQueue(k, new Comparator<struct2>() {//这里注意comparator用法这里写上范型,默认没写的那么o1和o2就只能是Object类型了
+            @Override
+            public int compare(struct2 o1, struct2 o2) {
+                return o2.frequency-o1.frequency;//o1-o2的话就是升序了,要的是降序
+            }
+        });
+        for(int n:nums){
+           if(!map.containsKey(n)){
+               struct2 s=new struct2(n,1);
+               map.put(n,s);
+           }else{
+               map.get(n).frequency++;
+           }
+        }
+
+        Iterator it=map.keySet().iterator();
+        while(it.hasNext()){
+            pq.offer(map.get(it.next()));
+        }
+        for(int i=0;i<k;i++){
+            rs.add(pq.poll().k);
+        }
+        return rs;
+    }
+}
+class struct2 {
+    int k;
+    public int frequency;
+    public struct2(int a,int b){
+        k=a;
+        frequency=b;
+    }
+}
