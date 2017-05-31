cb960710dc02833e6b3ccfd71185966943fd4889
Yufeng Zhu <yufeng.zhu@ge.com>
2017-05-31 16:49:33 -0700

    added DecodeString.java LongestSubstringwithAtLeastKRepeatingCharacters.java

---
+++ DecodeString.java
@@ -0,0 +1,119 @@
+import java.util.Stack;
+
+/**
+ * Created by 502575560 on 5/25/17.
+ */
+public class DecodeString {
+
+    public static void main(String[] args){
+//        String s="3[a]3[g2[bc2[d]2[kj]]]";
+        System.out.println(DecodeString.decodeString("2[2[2[b]]]"));//注意sd2[f2[2]g]i这样的字母前面没有数字的例子
+    }
+
+    //看这个3[a]3[g2[bc4[d]2[kj]]]例子想,tag用stack,自己想的,改了很久终于accept了,
+    //http://www.cnblogs.com/grandyang/p/5849037.html
+    //http://blog.csdn.net/mebiuw/article/details/52448807
+    //http://blog.csdn.net/qq508618087/article/details/52439114
+    public static String decodeString(String s) {
+        int i=0;
+        String rs="";
+        Stack<numString> st=new Stack<>();
+
+        while (i<s.length()){
+            char c=s.charAt(i);
+            if(c==']'){
+                i++;
+                numString ns=st.peek();
+                int num=ns.num;
+                String l=ns.s;
+
+                if(num!=-1){
+                    String temp="";
+                    for(int j=0;j<num;j++){
+                        temp+=l;
+                    }
+                    st.pop();
+                    if(st.size()==0){
+                        rs+=temp;
+                    }else{
+                        st.push(new numString(-1,temp));
+                    }
+                } else {
+                    String temp="";
+                    while (num==-1){
+                        temp=l+temp;
+                        st.pop();
+                        if(st.size()>0){
+                            num=st.peek().num;
+                            l=st.peek().s;
+                        }else {
+                            break;
+                        }
+                    }
+                    temp=l+temp;
+                    String temp2="";
+                    for(int j=0;j<num;j++){
+                        temp2+=temp;
+                    }
+                    st.pop();
+                    if(st.size()==0){
+                        rs+=temp2;
+                    }else{
+                        st.push(new numString(-1,temp2));
+                    }
+                }
+            }else {
+                String num="";
+                while (c-'0'>=0&&c-'9'<=0){
+                    num+=c;
+                    i++;
+                    c=s.charAt(i);
+                }
+                if(num==""){//说明是没有数字的
+                    StringBuilder letter=new StringBuilder();
+                    int count=0;
+                    while (i<s.length()){
+                        c=s.charAt(i);
+                        if(c==']'||c=='['||(c-'0'>0&&c-'9'<=0)){
+                            break;
+                        }
+                        letter.append(c);
+                        i++;
+                        count++;
+                    }
+                    //把原字符串加上[]
+                    String s2=s.substring(i);
+
+                    s=s.substring(0,i)+"]"+s2;
+                    s2=s.substring(i-count);
+                    s=s.substring(0,i-count)+"["+s2;
+                    st.push(new numString(1,letter.toString()));
+                    i+=1;
+                }else {
+                    //数组读完之后下一个肯定是'['
+                    i++;
+                    StringBuilder letter=new StringBuilder();
+                    while (true){
+                        c=s.charAt(i);
+                        if(c==']'||c=='['||(c-'0'>0&&c-'9'<=0)){
+                            break;
+                        }
+                        letter.append(c);
+                        i++;
+                    }
+                    st.push(new numString(Integer.parseInt(num),letter.toString()));
+                }
+            }
+        }
+
+        return rs;
+    }
+    static class numString{
+        int num;
+        String s;
+        public numString(int num, String s){
+            this.num=num;
+            this.s=s;
+        }
+    }
+}
---
+++ LongestSubstringwithAtLeastKRepeatingCharacters.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+/**
+ * Created by 502575560 on 5/31/17.
+ */
+public class LongestSubstringwithAtLeastKRepeatingCharacters {
+    public static void main(String[] args){
+        System.out.println(LongestSubstringwithAtLeastKRepeatingCharacters.longestSubstring("aaabb",3));
+    }
+    //自己也想不出来好的方法,看别人思路,递归那还是有点不好想
+    //http://www.cnblogs.com/reboot329/p/5875863.html
+    //http://www.cnblogs.com/grandyang/p/5852352.html 太高级看不懂
+    public static int longestSubstring(String s, int k) {
+        if(k<=1){
+            return s.length();
+        }
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(map.containsKey(c)){
+                map.put(c,map.get(c)+1);
+            }else{
+                map.put(c,1);
+            }
+        }
+        Set<Character> set2=new HashSet<>();
+        ArrayList<Character> al=new ArrayList<>();
+        Set<Character> set=map.keySet();
+        Iterator<Character> it=set.iterator();
+        while (it.hasNext()){
+            char c=it.next();
+            if(map.get(c)<k){
+                set2.add(c);
+            }
+        }
+        if(set2.size()==0){//少了这个判断就会爆栈
+            return s.length();
+        }
+        int rs=0;
+        int start=0;
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(set2.contains(c)){
+                start=i+1;
+                continue;
+            }else{
+                rs=Math.max(rs,longestSubstring(s.substring(start,i+1),k));
+            }
+
+        }
+        return rs;
+    }
+}
